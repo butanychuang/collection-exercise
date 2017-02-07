5bc47eb45169145f0e02e0b5c9496f1eadb8c442
butanychuang <butanychuang@kkbox.com>
2017-02-07 15:56:00 +0800

    Implementation

--- src/Transformer.php
+++ src/Transformer.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Support\Collection;
+
 class Transformer {
     public function transform($records, $meta) {
-        return [];
+        $meta = collect($meta)->map(function ($item) {
+            return collect($item)->keyBy('id');
+        });
+
+        $attributes = array('company', 'class', 'type');
+
+        return collect($attributes)->mapWithKeys(function ($attribute) use ($records, $meta) {
+            return [$attribute => collect($records)->pluck($attribute)->unique()->values()->map(function ($id) use ($attribute, $meta) {
+                return $meta[$attribute][$id];
+            })];
+        })->toArray();
     }
 }
